0637d8cac5952a205eae22f9565d05a2dc625d01
Vyachaslav Kuzharov <marhuz.61@gmail.com>
2016-06-16 00:47:03 +0300

    Change in bootstrap configure

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module SpaTemplate
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    # Precompile additional asset types
     config.assets.precompile += %w( auth.css auth.js )
   end
 end
